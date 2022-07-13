0d58071115147c250cd0f18664e71408bd488cf4
Zukhra <z.malikova114@gmail.com>
2022-07-13 23:45:24 +0600

    Homework

---
+++ seminar1/Program.cs
@@ -0,0 +1,77 @@
+﻿/* TASK 1
+Console.Write("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+}
+if (num1 > num2)
+{
+    Console.WriteLine("min = " + num2);
+}
+else
+{
+    Console.WriteLine("min = " + num1);
+}
+*/
+
+/* TASK 2
+Console.Write("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("max = " + max); 
+*/
+
+/* TASK 3
+Console.Write("Enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine(num + " this even number");
+}
+else
+{
+    Console.WriteLine(num + " this not even number");
+}
+*/
+/*
+Console.Write("Enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = num * (-1);
+
+while (current % 2 == num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+/* TASK 4
+Console.Write("Enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 2;
+while (res < num)
+{
+
+    Console.Write(res + " ");
+    res+=2;
+}
+*/
